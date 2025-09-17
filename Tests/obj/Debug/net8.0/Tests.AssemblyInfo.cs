538af7da519577634b4fc37d44769b32c9815c6c
Eduardo correa dos Santos <eduardocorrea82@hotmail.com>
2025-09-17 11:17:31 -0300

    0.0.0.13

--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b16638350862f39e5c12e772d54b062c9a2766c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1c9038609ab5441def10aa00d75aeda1e028960")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
